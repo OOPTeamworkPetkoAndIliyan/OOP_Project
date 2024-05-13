e4916175852ba1475e71fe90cc16bff3de77c25c
petkoAs95 <petko.asenov.a60@learn.telerikacademy.com>
2024-05-13 13:42:51 +0300

    adding MemberImpl without showActivity

--- src/com/company/oop/taskManagement/models/contracts/Member.java
+++ src/com/company/oop/taskManagement/models/contracts/Member.java
@@ -1,6 +1,9 @@
 package com.company.oop.taskManagement.models.contracts;
 
+import java.util.List;
+
 public interface Member  extends ShowActivity{
     void addTask(Task task);
     void removeTask(Task task);
+    List<Task> getTasks();
 }
