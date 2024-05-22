18f1349e15cbf9fdf7b01231466b3072843de811
IHKaragyozov19 <ilian_k_19@abv.bg>
2024-05-22 23:45:43 +0300

    Implementing CreateNewBugWithAssigneeCommandTests, CreateNewBugWithoutAssigneeCommandTest, CreateNewFeedbackCommandTests.

--- tests/com/company/oop/taskManagement/models/commands/CreateNewBugWithoutAssigneeCommandTest.java
+++ tests/com/company/oop/taskManagement/models/commands/CreateNewBugWithoutAssigneeCommandTest.java
@@ -1,7 +1,7 @@
 package com.company.oop.taskManagement.models.commands;
 
-import com.company.oop.taskManagement.commands.CreateNewBoardCommand;
 import com.company.oop.taskManagement.commands.CreateNewBugWithAssigneeCommand;
+import com.company.oop.taskManagement.commands.CreateNewBugWithoutAssigneeCommand;
 import com.company.oop.taskManagement.commands.contracts.Command;
 import com.company.oop.taskManagement.core.TaskManagerRepositoryImpl;
 import com.company.oop.taskManagement.core.contracts.TaskManagerRepository;
@@ -9,7 +9,6 @@ import com.company.oop.taskManagement.models.Utils.CreateBaseConstants;
 import com.company.oop.taskManagement.models.Utils.UtilsTest;
 import com.company.oop.taskManagement.models.contracts.Board;
 import com.company.oop.taskManagement.models.contracts.Member;
-import com.company.oop.taskManagement.models.contracts.Team;
 import com.company.oop.taskManagement.models.enums.Priority;
 import com.company.oop.taskManagement.models.enums.Severity;
 import org.junit.jupiter.api.Assertions;
@@ -20,25 +19,24 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-public class CreateNewBugWithAssigneeCommandTests {
-    public static final int EXPECTED_NUMBER_OF_ARGUMENTS = 5;
+public class CreateNewBugWithoutAssigneeCommandTest {
+    public static final int EXPECTED_NUMBER_OF_ARGUMENTS = 4;
     private Command command;
     private TaskManagerRepository taskManagerRepository;
     @BeforeEach
     public void before() {
         this.taskManagerRepository = new TaskManagerRepositoryImpl();
-        this.command = new CreateNewBugWithAssigneeCommand(taskManagerRepository);
+        this.command = new CreateNewBugWithoutAssigneeCommand(taskManagerRepository);
         Board board = taskManagerRepository.createBoard(CreateBaseConstants.NAME_BOARD_VALID_LENGTH);
         Member member = taskManagerRepository.createMember(CreateBaseConstants.NAME_MEMBER_VALID_LENGTH);
     }
     @Test
-    public void execute_Should_CreateNewBugWithAssignee_When_ValidInput(){
+    public void execute_Should_CreateNewBugWithoutAssignee_When_ValidInput(){
         List<String> params = List.of(
                 CreateBaseConstants.VALID_TITLE,
                 CreateBaseConstants.VALID_DESCRIPTION,
                 Priority.HIGH.toString(),
-                Severity.CRITICAL.toString(),
-                CreateBaseConstants.NAME_MEMBER_VALID_LENGTH
+                Severity.CRITICAL.toString()
         );
         command.execute(params);
         Assertions.assertEquals(1, taskManagerRepository.getBugs().size());
@@ -52,13 +50,12 @@ public class CreateNewBugWithAssigneeCommandTests {
         assertThrows(IllegalArgumentException.class, () -> command.execute(params));
     }
     @Test
-    public void execute_Should_CreateNewBugWithAssigneeInBoard_When_ValidInput(){
+    public void execute_Should_CreateNewBugWithoutAssigneeInBoard_When_ValidInput(){
         List<String> params = List.of(
                 CreateBaseConstants.VALID_TITLE,
                 CreateBaseConstants.VALID_DESCRIPTION,
                 Priority.HIGH.toString(),
                 Severity.CRITICAL.toString(),
-                CreateBaseConstants.NAME_MEMBER_VALID_LENGTH,
                 CreateBaseConstants.NAME_BOARD_VALID_LENGTH
         );
         command.execute(params);
--- tests/com/company/oop/taskManagement/models/commands/CreateNewFeedbackCommandTests.java
+++ tests/com/company/oop/taskManagement/models/commands/CreateNewFeedbackCommandTests.java
@@ -1,15 +1,13 @@
 package com.company.oop.taskManagement.models.commands;
 
-import com.company.oop.taskManagement.commands.CreateNewBoardCommand;
 import com.company.oop.taskManagement.commands.CreateNewBugWithAssigneeCommand;
+import com.company.oop.taskManagement.commands.CreateNewFeedbackCommand;
 import com.company.oop.taskManagement.commands.contracts.Command;
 import com.company.oop.taskManagement.core.TaskManagerRepositoryImpl;
 import com.company.oop.taskManagement.core.contracts.TaskManagerRepository;
 import com.company.oop.taskManagement.models.Utils.CreateBaseConstants;
 import com.company.oop.taskManagement.models.Utils.UtilsTest;
 import com.company.oop.taskManagement.models.contracts.Board;
-import com.company.oop.taskManagement.models.contracts.Member;
-import com.company.oop.taskManagement.models.contracts.Team;
 import com.company.oop.taskManagement.models.enums.Priority;
 import com.company.oop.taskManagement.models.enums.Severity;
 import org.junit.jupiter.api.Assertions;
@@ -20,28 +18,25 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-public class CreateNewBugWithAssigneeCommandTests {
-    public static final int EXPECTED_NUMBER_OF_ARGUMENTS = 5;
+public class CreateNewFeedbackCommandTests {
+    public static final int EXPECTED_NUMBER_OF_ARGUMENTS = 3;
     private Command command;
     private TaskManagerRepository taskManagerRepository;
     @BeforeEach
     public void before() {
         this.taskManagerRepository = new TaskManagerRepositoryImpl();
-        this.command = new CreateNewBugWithAssigneeCommand(taskManagerRepository);
+        this.command = new CreateNewFeedbackCommand(taskManagerRepository);
         Board board = taskManagerRepository.createBoard(CreateBaseConstants.NAME_BOARD_VALID_LENGTH);
-        Member member = taskManagerRepository.createMember(CreateBaseConstants.NAME_MEMBER_VALID_LENGTH);
     }
     @Test
-    public void execute_Should_CreateNewBugWithAssignee_When_ValidInput(){
+    public void execute_Should_CreateNewFeedback_When_ValidInput(){
         List<String> params = List.of(
                 CreateBaseConstants.VALID_TITLE,
                 CreateBaseConstants.VALID_DESCRIPTION,
-                Priority.HIGH.toString(),
-                Severity.CRITICAL.toString(),
-                CreateBaseConstants.NAME_MEMBER_VALID_LENGTH
+                String.valueOf(CreateBaseConstants.FEEDBACK_RATING)
         );
         command.execute(params);
-        Assertions.assertEquals(1, taskManagerRepository.getBugs().size());
+        Assertions.assertEquals(1, taskManagerRepository.getFeedbacks().size());
     }
     @Test
     public void should_ThrowException_When_ArgumentCountDifferentThanExpected() {
@@ -56,13 +51,10 @@ public class CreateNewBugWithAssigneeCommandTests {
         List<String> params = List.of(
                 CreateBaseConstants.VALID_TITLE,
                 CreateBaseConstants.VALID_DESCRIPTION,
-                Priority.HIGH.toString(),
-                Severity.CRITICAL.toString(),
-                CreateBaseConstants.NAME_MEMBER_VALID_LENGTH,
-                CreateBaseConstants.NAME_BOARD_VALID_LENGTH
+               String.valueOf(CreateBaseConstants.FEEDBACK_RATING)
         );
         command.execute(params);
-        Assertions.assertEquals(1, taskManagerRepository.getBugs().size());
+        Assertions.assertEquals(1, taskManagerRepository.getFeedbacks().size());
         Assertions.assertEquals(1, taskManagerRepository.getBoards().size());
     }
 }
